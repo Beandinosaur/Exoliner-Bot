2ac2f916677f4619364bc34c2470a04c16fd25cd
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-01-20 01:49:16 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,15 +7,27 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  
-  if (message.content.indexOf(prefix) !== 0) return;
-	var sender = message.author
-	let args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-  
-  if (command === 'buy') {
-    message.channel.send(`your mom gay lol`)
-  }
+if (message.content.indexOf(prefix) !== 0) return;
+	
+var sender = message.author
+let args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+	
+if (command === 'buy') {
+	message.channel.send(`your mom gay lol`)
+}
+	
+if (command == 'whitelist') {
+	if (!message.member.permissions.has('ADMINISTRATOR')) return;
+	if (!args[0]) return message.channel.send('who are you trying to  whitelist noob')
+	let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if (!member) return message.channel.send('who are you trying to  whitelist noob');
+	let role = message.guild.roles.find('name', 'Whitelisted');
+	if (!role) return message.channel.send('could not find the whitelisted role ahhhh');
+	
+	member.addRole(role.id);
+	message.channel.send(`Successfully whitelisted ${member.user.tag}`);
+}
   
 });
 
