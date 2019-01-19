a5c6e1aa113405518375f38ed8fbb968ada3240f
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-01-20 01:53:06 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,14 +19,14 @@ if (command === 'buy') {
 	
 if (command == 'whitelist') {
 	if (!message.member.permissions.has('ADMINISTRATOR')) return;
-	if (!args[0]) return message.channel.send('who are you trying to  whitelist noob')
+	if (!args[0]) return message.channel.send('who are you trying to whitelist noob')
 	let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-	if (!member) return message.channel.send('who are you trying to  whitelist noob');
+	if (!member) return message.channel.send('who are you trying to whitelist noob');
 	let role = message.guild.roles.find('name', 'Whitelisted');
 	if (!role) return message.channel.send('could not find the whitelisted role ahhhh');
 	
 	member.addRole(role.id);
-	message.channel.send(`Successfully whitelisted ${member.user.tag}`);
+	message.channel.send(`${member.user.tag} was successfully whitelisted`);
 }
   
 });
