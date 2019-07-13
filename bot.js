68207ca640437557aca88eb75dc7b751f6c6d608
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-13 20:03:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,12 @@ if (command === 'request') {
 	if (message.channel.type == "dm") {
 		let username = args[0]
 		let owner = message.author
-		message.channel.send(`Sent request for account ${username}.`)
+		
+		if (!args[0]) {
+			return message.channel.send('You did not specify a user to request a whitelist for.')
+		}
+		
+		message.channel.send(`Sent whitelist request for account ${username}.`)
 		
 		channel.send(`User ${owner} sent a whitelist request for username '${username}'.`)
 		.then(function (message) {
@@ -27,7 +32,20 @@ if (command === 'request') {
 		})
 	}
 }
-  
+
+if (command == 'whitelist') {
+	if (!message.member.permissions.has('ADMINISTRATOR')) {
+	    return message.channel.send('You do not have permissions to use this command.')
+	}
+	
+	if (!args[0]) {
+		return message.channel.send('You did not specify a user to whitelist.')
+	}
+	
+	let username = args[0]
+	message.channel.send(`Successfully whitelisted user ${user}`);
+}
+
 });
 
 client.login(process.env.BOT_TOKEN);
