3538db437a39c99daca687ee405b4c2c4ed60800
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-13 19:42:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,23 +30,6 @@ if (command === 'request') {
 		
 		channel.send(`User ${sender} sent a whitelist request for username '${username}'.`).then(async message => {
 			message.react('✅').then(() => message.react('❌'));
-			
-			
-			
-			message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-				.then(collected => {
-				const reaction = collected.first();
-				if (reaction.emoji.name === '✅') {
-					message.reply('you reacted with a thumbs up.');
-				}
-				else {
-					message.reply('you reacted with a thumbs down.');
-				}
-			})
-			.catch(collected => {
-				console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-				message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
-			});
 		})
 	}
 }
