c8f6e9adafad7bbfbeba83ddb21cb73253de0946
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-13 19:01:21 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,16 +13,14 @@ var sender = message.author
 let args = message.content.slice(prefix.length).split(/ +/);
 let channel = client.channels.get("599542199998349312")
 const command = args.shift().toLowerCase();
-
+	
 if (command === 'request') {
 	if (message.channel.type == "dm") {
 		let username = args[0]
-		if (!args[0]) return message.channel.send('You did not specify a user to whitelist.')
-		
-		let owner = message.author.tag
+		let owner = message.author
 		message.channel.send(`Sent request for account ${username}.`)
 		
-		channel.send(`***User ${owner} sent a whitelist request for username ${username}***.`)
+		channel.send(`User ${owner} sent a whitelist request for username '${username}'.`)
 		.then(function (message) {
 			message.react('✅')
 			message.react('❌')
@@ -31,10 +29,18 @@ if (command === 'request') {
 }
 	
 if (command == 'whitelist') {
-	if (!message.member.permissions.has('ADMINISTRATOR') return message.channel.send('You do not have permissions to use that command.')
-	if (!args[0]) return message.channel.send('You did not specify a user to whitelist.')
+	if (!message.member.permissions.has('ADMINISTRATOR')) return;
+	if (!args[0]) return message.channel.send('who are you trying to whitelist noob')
+	let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+	if (!member) return message.channel.send('who are you trying to whitelist noob');
+	let role = message.guild.roles.find('name', 'Whitelisted');
+	if (!role) return message.channel.send('could not find the whitelisted role ahhhh');
 	
-	message.channel.send(`***${args[0]} was successfully whitelisted.***`);
+	if(message.member.roles.find("name", "Whitelisted")) {
+		return message.channel.send('that user is already whitelisted are u high')
+	}
+	member.addRole(role.id);
+	message.channel.send(`***${member.user.tag} was successfully whitelisted***`);
 }
   
 });
