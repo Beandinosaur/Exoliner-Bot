ddf1e505eb2b881e767c6ecd53cdc6ee85e46a37
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-17 17:39:26 +0800

    Update denyWl.js

--- denyWl.js
+++ denyWl.js
@@ -1,1 +1,8 @@
+const Discord = require('discord.js');
 
+async function acceptWl(whitelister, user) {
+  acceptWlChannel.send(`Whitelist request from **${username}** has been denied.`);
+  newMessage.delete()
+}
+
+module.exports = denyWl;
