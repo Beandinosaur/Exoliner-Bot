f99b37def526870f2a54b9c3f51ae67a3ea521ba
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-17 17:36:59 +0800

    Update acceptWl.js

--- acceptWl.js
+++ acceptWl.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 
 async function acceptWl(whitelister, user) {
-  acceptWlChannel.send(`Whitelist request from **${username}** has been accepted.`);
-				newMessage.delete()
+	acceptWlChannel.send(`Whitelist request from **${username}** has been accepted.`);
+	newMessage.delete()
 				
-				var embed = new Discord.RichEmbed();
-				embed
-					.setColor("#0080FF")
-					.setAuthor(`Whitelist Request`)
-					.addField(`User Whitelisted:`, `${whitelister}`)
-					.addField(`Accepted by:`, `${user}`)
-				
-				WlLogs.send(embed)
+	var embed = new Discord.RichEmbed();
+	embed
+		.setColor("#0080FF")
+		.setAuthor(`Whitelist Request`)
+		.addField(`User Whitelisted:`, `${whitelister}`)
+		.addField(`Accepted by:`, `${user}`)
+	
+	WlLogs.send(embed)
 }
 
 module.exports = acceptWl;
