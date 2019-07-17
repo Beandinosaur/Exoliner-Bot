907cf8eb6ebb10741422f6e9d351e9a9db01787a
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-17 17:47:55 +0800

    Update denyWl.js

--- denyWl.js
+++ denyWl.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 async function denyWl(whitelister, user) {
+  
+  let acceptWlChannel = client.channels.get("599542199998349312")
+	let WlLogs = client.channels.get("599583754587078676")
+  
   acceptWlChannel.send(`Whitelist request from **${user}** has been denied.`);
 }
 
