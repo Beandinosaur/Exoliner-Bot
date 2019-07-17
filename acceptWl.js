7ba9327672781479b695bdd2eb51d04e2a47fb7f
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-17 18:13:27 +0800

    Update acceptWl.js

--- acceptWl.js
+++ acceptWl.js
@@ -5,7 +5,7 @@ async function acceptWl(whitelister, user, client) {
 	let acceptWlChannel = client.channels.get("599542199998349312")
 	let WlLogs = client.channels.get("599583754587078676")
 
-	acceptWlChannel.send(`Whitelist request from **${user}** has been accepted.`);
+	acceptWlChannel.send(`Whitelist request from **${user}** has been accepted.`)
 	.then(message => {
 		message.delete(5000)
 	};
