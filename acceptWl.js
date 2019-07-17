b1da6eff1165f39fa1d5f6bd45c6fcdd8a7c468c
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-17 18:11:37 +0800

    Update acceptWl.js

--- acceptWl.js
+++ acceptWl.js
@@ -6,7 +6,10 @@ async function acceptWl(whitelister, user, client) {
 	let WlLogs = client.channels.get("599583754587078676")
 
 	acceptWlChannel.send(`Whitelist request from **${user}** has been accepted.`);
-				
+	.then(message => {
+		message.delete(5000)
+	};
+	       
 	const Embed = new Discord.RichEmbed();
 	Embed
 		.setColor("#0080FF")
