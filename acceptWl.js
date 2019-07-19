841cba3909694958718f47ba799a4fbbe88d5e9b
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-19 18:17:49 +0800

    Update acceptWl.js

--- acceptWl.js
+++ acceptWl.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
+const request = require('request');
 const Color = "#0080FF";
 
 async function acceptWl(whitelister, user, owner, client) {
 	
 	let acceptWlChannel = client.channels.get("599542199998349312")
 	let WlLogs = client.channels.get("599583754587078676")
+	
+	
+	request(`http://whatareyoulookingatsir.000webhostapp.com/requestWL.php?name=${user}&key=process.env.SITE_KEY`, function (error, response, body) {
+		console.log('error:', error); // Print the error if one occurred
+		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+		console.log('body:', body); // Print the HTML for the Google homepage.
+	});
 
+	
 	acceptWlChannel.send(`Whitelist request from **${user}** has been accepted.`)
 	.then(message => {
 		message.delete(5000)
