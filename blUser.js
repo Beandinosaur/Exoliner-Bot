849b413c81a010c5199641bc3b76b178f918edb2
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-20 16:05:05 +0800

    Update blUser.js

--- blUser.js
+++ blUser.js
@@ -4,9 +4,16 @@ const sitekey = process.env.SITE_KEY
 
 async function blUser(whitelister, user, client) {
 	
-	let acceptWlChannel = client.channels.get("599542199998349312")
-	let WlLogs = client.channels.get("599583754587078676")
 	
+	var acceptWlChannel = client.channels.get("599542199998349312")
+	var WlLogs = client.channels.get("601975027230834690")
+	var sitekey = process.env.SITE_KEY
+	
+	if (server == 586091288743641131) { // exo
+		var acceptWlChannel = client.channels.get("599542199998349312")
+		var WlLogs = client.channels.get("599583754587078676")
+		var sitekey = process.env.EXO_KEY
+	}
 	
 	request(`http://whatareyoulookingatsir.000webhostapp.com/removeWL.php?name=${user}&key=${sitekey}`, function (error, response, body) {
 		console.log('error:', error); // Print the error if one occurred
