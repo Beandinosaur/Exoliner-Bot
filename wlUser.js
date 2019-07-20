7cd47078fa0ac4611bee6500f55d0488b862113d
Beandinosaur <46848009+Beandinosaur@users.noreply.github.com>
2019-07-20 11:13:27 +0800

    Update wlUser.js

--- wlUser.js
+++ wlUser.js
@@ -1,13 +1,20 @@
 const Discord = require('discord.js');
 const request = require('request');
-const sitekey = process.env.SITE_KEY
-const exokey = process.env.EXO_KEY
 
 async function wlUser(whitelister, user, client, message) {
-
-	let acceptWlChannel = client.channels.get("599542199998349312")
-	let WlLogs = client.channels.get("599583754587078676")
 	
+	var server = message.guild.id;
+	
+	var acceptWlChannel = client.channels.get("599542199998349312")
+	var WlLogs = client.channels.get("599583754587078676")
+	var sitekey = process.env.SITE_KEY
+	
+	if (server == 586091288743641131) { // exo
+		var acceptWlChannel = client.channels.get("599542199998349312")
+		var WlLogs = client.channels.get("599583754587078676")
+		var sitekey = process.env.EXO_KEY
+	}
+
 	
 	request(`http://whatareyoulookingatsir.000webhostapp.com/requestWL.php?name=${user}&key=${sitekey}`, function (error, response, body) {
 		console.log('error:', error); // Print the error if one occurred
